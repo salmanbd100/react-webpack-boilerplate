e7cf7a7bf8cf02cc3debbafae9e5fa16134c16ec
Salman Rahman <salman@Salmans-MacBook-Pro.local>
2019-04-27 11:33:05 +0600

    config change

--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
-import React from 'react'
-import ReactDom from 'react-dom'
-import App from './app/App'
-import './assets/scss/style.scss'
+import React from "react";
+import ReactDOM from 'react-dom';
+import App from "./app/App";
+import "./assets/scss/style.scss";
 
-ReactDOM.render(
-    <App />,
-    document.getElementById('app')
-)
+ReactDOM.render(<App />, document.getElementById("app"));
